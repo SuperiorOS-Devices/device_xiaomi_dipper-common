fba312e24fbd4f17077b0d09a7f9fc865640e097
argraur <argraur@gmail.com>
2018-11-02 09:46:39 +0000

    overlay: rename lineage overlays to overlay-aosip
    Change-Id: Iecccade8e7b9ef6bfacace150a74026646e11422
    Signed-off-by: argraur <argraur@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -17,7 +17,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vend
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
